fc0601a8c8966557971fa0cc662b384546a97b6f
di-shykh <di49@mail.ru>
2024-11-12 16:22:02 +0300

    feat:add all functions

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -482,6 +482,11 @@ function encodeToRot13(str) {
   const alphabetStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   const rot13AlphabetStr = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
   let tempArr = str.split('');
+  let newArr = tempArr.map(item => {
+    if (alphabetStr.indexOf(item) !== -1) return rot13AlphabetStr[alphabetStr.indexOf(item)];
+    else return item;
+  });
+  return newArr.join('');
 }
 
 /**
@@ -508,8 +513,14 @@ function encodeToRot13(str) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId( /* value */ ) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  let arrayOfCards = [
+    'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'
+  ];
+  return arrayOfCards.indexOf(value);
 }
 
 module.exports = {
